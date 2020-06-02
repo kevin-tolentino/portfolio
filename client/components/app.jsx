7219f6fc90330f7c1802ffe97eb159a95654a5d0
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-01 18:47:47 -0700

    got initial bootstrap Navbar component to display by adding bootstrap and bootstrap react as a dependency and using template from documentation

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import Navigation from './navigation';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null,
-      isLoading: true
     };
   }
+
   render() {
     return (
-      <h1>hello!</h1>
-    )
+      <Navigation />
+    );
   }
 }
