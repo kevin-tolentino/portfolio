620d3cb39e05d334789113cb7a52a6931f67336c
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-02 11:19:22 -0700

    server/public/images/
    added initial skills icons

---
+++ client/components/skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Skills(props) {
+  return (
+    <div className="container-xl bg-light">
+      <div className="row">
+        <div className="p-3 col">
+          <h2>Technical Skills</h2>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Skills;
