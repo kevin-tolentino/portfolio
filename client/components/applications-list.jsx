920e71a833a6ccdfd7edece7c48f87d790a56769
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-03 10:08:49 -0700

    base functionality of app card sucessful

--- client/components/applications-list.jsx
+++ client/components/applications-list.jsx
@@ -13,9 +13,15 @@ function ApplicationListItem(props) {
   return (
     <div className="p-3 card col-md-4">
       <img className="card-img-top app-img" src={props.demo} alt={props.name}/>
-      <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-        <p className="card-text">{props.desc}</p>
+      <div className="card-body d-flex flex-column">
+        <div>
+          <h5 className="card-title">{props.name}</h5>
+          <p className="card-text">{props.desc}</p>
+        </div>
+        <div className="d-flex justify-content-center">
+          <a className="m-1 btn btn-primary" href={props.gitHub} target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a className="m-1 btn btn-secondary" href={props.live} target="_blank" rel="noopener noreferrer">Live</a>
+        </div>
       </div>
       <div className="card-footer">
         {techItems}
@@ -33,7 +39,8 @@ function ApplicationList(props) {
           <ApplicationListItem key={currentApp.name}
             demo={currentApp.demo}
             name={currentApp.name}
-            desc={currentApp.desc} gitHub={props.github}
+            desc={currentApp.desc}
+            gitHub={currentApp.gitHub}
             live={currentApp.live}
             tech={currentApp.tech}/>
         );
