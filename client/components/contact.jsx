65d5973b1729924dcdc0ec7e955f6440b8087cf8
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-03 14:31:12 -0700

    added 3rd row for contact icons

--- client/components/contact.jsx
+++ client/components/contact.jsx
@@ -13,6 +13,26 @@ function Contact(props) {
           <h3 className="text-center">Kevin Tolentino</h3>
         </div>
       </div>
+      <div className="row my-4">
+        <div className="col">
+          <div className="text-center">
+            {/* add location icon */}
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="text-center">
+            {/* add phone icon */}
+            <a href="tel:562-303-7446">(562) 303-7446</a>
+          </div>
+        </div>
+        <div className="col">
+          <div className="text-center">
+            {/* add email icon */}
+            <a href="mailto:kevin.tolentino95@gmail.com">kevin.tolentino95@gmail.com</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
