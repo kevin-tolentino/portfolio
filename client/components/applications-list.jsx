23e91df86ce201041f4e72476faefeb663bac946
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-03 09:52:16 -0700

    implemented a for loop to iterate through props.tech array to display formatted text

--- client/components/applications-list.jsx
+++ client/components/applications-list.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
 function ApplicationListItem(props) {
+  const techItems = [];
+  for (let techIterator = 0; techIterator < props.tech.length; techIterator++) {
+    if (techIterator === props.tech.length - 1) {
+      techItems.push(props.tech[techIterator]);
+    } else {
+      techItems.push(props.tech[techIterator] + ', ');
+    }
+  }
+
   return (
-    <div className="card col-md-4">
+    <div className="p-3 card col-md-4">
       <img className="card-img-top app-img" src={props.demo} alt={props.name}/>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
         <p className="card-text">{props.desc}</p>
       </div>
       <div className="card-footer">
-        {props.tech}
+        {techItems}
       </div>
     </div>
 
@@ -18,7 +27,7 @@ function ApplicationListItem(props) {
 
 function ApplicationList(props) {
   return (
-    <div className="row flex-wrap">
+    <div className="p-4 row flex-wrap">
       {props.applicationList.map(currentApp => {
         return (
           <ApplicationListItem key={currentApp.name}
