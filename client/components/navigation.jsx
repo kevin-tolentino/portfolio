2d88fcedd7dfa7562b5f6c31f34fa08215918dac
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-04 13:06:52 -0700

    replaces Link scroll component with a scroller callback instead

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link, scroller, animateScroll as scroll } from 'react-scroll';
 
 class Navigation extends React.Component {
   render() {
     return (
-
-      <Navbar className="justify-content-between" bg="dark" variant="dark" sticky="top" expand="sm">
+      <Navbar
+        collapseOnSelect
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        expand="sm">
         <Navbar.Brand><i className="fas fa-laptop-code"></i> Kevin Tolentino</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
           <Nav>
-            {/* Change this component later to <Link> component when implementing
-            the react-scroll dependency */}
-            <Nav.Link>About Me</Nav.Link>
+            <Nav.Link href="">About Me</Nav.Link>
             <Nav.Link>Skills</Nav.Link>
             <Nav.Link>Tools</Nav.Link>
             <Nav.Link>Applications</Nav.Link>
-            <Nav.Link><Link activeClass="active" className="test1" to="contact" spy={true} smooth={true} duration={500} >Contact</Link></Nav.Link>
+            <Nav.Item>
+              <Link onClick={() => {
+                scroller.scrollTo('contact', {
+                  durration: 800,
+                  delay: 0,
+                  smooth: 'easeInOutQuart'
+                });
+              }}
+              // activeClass="active"
+              //   className=""
+              //   to="contact"
+              //   spy={true}
+              //   smooth={true}
+              //   duration={500}
+              >Contact</Link>
+            </Nav.Item>
+
           </Nav>
         </Navbar.Collapse>
       </Navbar>
