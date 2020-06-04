a8137ca0beb16b33abc71c95e70c8dae256dee41
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-04 15:19:13 -0700

    set an ternary conditional to return different offset value for scrollTo method

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -7,7 +7,14 @@ class Navigation extends React.Component {
   render() {
     return (
       <Navbar
-        onSelect={() => { console.log('yes'); }}
+        collapseOnSelect
+        onSelect={selectedKey => {
+          scroller.scrollTo(selectedKey, {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+          });
+        }}
         bg="dark"
         variant="dark"
         sticky="top"
@@ -16,33 +23,11 @@ class Navigation extends React.Component {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
           <Nav>
-            <Nav.Link eventKey='about-me' onClick={() => {
-              scroller.scrollTo('contact', {
-                duration: 800,
-                delay: 0,
-                smooth: 'easeInOutQuart'
-              });
-            }}>About Me</Nav.Link>
-            <Nav.Link>Skills</Nav.Link>
-            <Nav.Link>Tools</Nav.Link>
-            <Nav.Link>Applications</Nav.Link>
-            <Nav.Item>
-              <Link onClick={() => {
-                scroller.scrollTo('contact', {
-                  duration: 800,
-                  delay: 0,
-                  smooth: 'easeInOutQuart'
-                });
-              }}
-              // activeClass="active"
-              //   className=""
-              //   to="contact"
-              //   spy={true}
-              //   smooth={true}
-              //   duration={500}
-              >Contact</Link>
-            </Nav.Item>
-
+            <Nav.Link eventKey="about-me">About Me</Nav.Link>
+            <Nav.Link eventKey="skills">Skills</Nav.Link>
+            <Nav.Link eventKey="tools">Tools</Nav.Link>
+            <Nav.Link eventKey="applications">Applications</Nav.Link>
+            <Nav.Link eventKey="contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
