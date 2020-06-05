c5e5ca01b5622ef6678850bc0f93a9438f10bd8d
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-04 18:34:42 -0700

    moved github calendar to application component added tooltip for react calendar by installing react-tooltips

--- client/components/hello.jsx
+++ client/components/hello.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hello(props) {
   return (
-    <div className="container-fluid bg-dark component d-flex flex-column justify-content-center">
+    <div className="container-fluid bg-dark component d-flex flex-column justify-content-center component">
       <div className="row">
         <div className="col">
           <h1 className="text-center">Hello! It&apos;s nice to meet you!</h1>
