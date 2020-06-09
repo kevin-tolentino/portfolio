19c9a832920bf56e7d5b5ece2b3f9a34ae5cda16
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-09 16:54:45 -0700

    changed hero background and row background color styling

--- client/components/hello.jsx
+++ client/components/hello.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Hello(props) {
   return (
-    <div className="container-fluid text-white component d-flex flex-column justify-content-center component hero-image">
-      <div className="row">
-        <div className="col">
-          <h1 className="text-center">Learn. Code. Serve.</h1>
+    <div className="text-white d-flex flex-column justify-content-center component hero-image">
+      <div className="hero-icon-row">
+        <div className='hero-icon-div d-flex flex-column align-items-center justify-content-center'>
+          <img className="hero-logo" src="/images/logo-white.png" alt="logo-white" />
+          <h1 className="text-center hero-text">Learn. Code. Serve.</h1>
         </div>
       </div>
 
