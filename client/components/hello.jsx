33aa35c232df9cfcd25cab0a74f324eb431b5f63
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-09 20:12:35 -0700

    added css transition for logo hover styling and started to work on scroll event.

--- client/components/hello.jsx
+++ client/components/hello.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { scroller } from 'react-scroll';
 
 class Hello extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.handleScroll = this.handleScroll.bind(this);
+  // }
+
+  // work on this tomorrrow
+  // handleScroll() {
+  //   console.log('hello');
+  // }
+
   render() {
+
     return (
       <div className="text-white d-flex flex-column justify-content-center component hero-image">
         <div className="hero-icon-row">
-          <div className='hero-icon-div d-flex flex-column align-items-center justify-content-center'>
+          <div onClick={() => {
+            scroller.scrollTo('about-me', {
+              duration: 800,
+              delay: 0,
+              smooth: 'easeInOutQuart'
+            });
+          }} className='cursor-pointer hero-icon-div d-flex flex-column align-items-center justify-content-center'>
             <div className="hero-logo-container">
               <img className="hero-logo" src="/images/logo-white.png" alt="logo-white" />
               <div className="hero-logo-div-sibling"></div>
