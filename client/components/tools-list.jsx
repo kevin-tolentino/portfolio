9bc5e6f17765e4f85d26b29ed0c6f5c2ab4dfdb3
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-06-17 11:05:43 -0700

    moved lazyload component to image tags as opposed to the div column

--- client/components/tools-list.jsx
+++ client/components/tools-list.jsx
@@ -3,16 +3,16 @@ import LazyLoad from 'react-lazyload';
 
 function ToolsListItem(props) {
   return (
-    <LazyLoad height={'100%'} offset={100} once>
-      <div className="fade-in-opacity col-6 col-md-4">
-        <div className="d-flex justify-content-center dev-icon-div">
+    <div className="fade-in-opacity col-6 col-md-4">
+      <div className="d-flex justify-content-center dev-icon-div">
+        <LazyLoad height={'100%'} offset={100} once>
           <img className="dev-icon" src={props.image.path} alt={props.image.name}/>
-        </div>
-        <div className="text-center">
-          <h6>{props.image.name}</h6>
-        </div>
+        </LazyLoad>
       </div>
-    </LazyLoad>
+      <div className="text-center">
+        <h6>{props.image.name}</h6>
+      </div>
+    </div>
   );
 }
 
