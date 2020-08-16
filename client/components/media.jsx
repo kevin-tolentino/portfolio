8a28df4a586710062a85cfc0dcdfcf44d748cdb1
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-15 22:20:59 -0700

    fixed div component styling for media section

--- client/components/media.jsx
+++ client/components/media.jsx
@@ -5,7 +5,14 @@ class MediaSection extends React.Component {
 
     return (
       <div id="MediaSection" className={`${this.props.background} 'pb-4 container component`}>
-        <div>
+        <div id="tools" className="p-3">
+          <div className="row">
+            <div className="p-3 col">
+              <h2>Media</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/2c-5Wc2EoLU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
       </div>
