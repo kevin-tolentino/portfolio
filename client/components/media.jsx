de234a3689fdd878d215d5568e099c83b1cfa534
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-08-17 16:31:11 -0700

    adjustsed div layers so that background color of media section extends full width of viewport

--- client/components/media.jsx
+++ client/components/media.jsx
@@ -4,16 +4,18 @@ class MediaSection extends React.Component {
   render() {
 
     return (
-      <div id="MediaSection" className={`${this.props.background} 'pb-4 container component`}>
-        <div id="tools" className="p-3">
-          <div className="row">
-            <div className="p-3 col">
-              <h2>Media</h2>
+      <div className={this.props.background}>
+        <div id="MediaSection" className="pb-4 container-xl text-white component">
+          <div id="media" className="p-3">
+            <div className="row">
+              <div className="p-3 col">
+                <h2>Media</h2>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/2c-5Wc2EoLU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <div className="row">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2c-5Wc2EoLU" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          </div>
         </div>
       </div>
     );
